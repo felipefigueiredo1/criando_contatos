283b2008311d03d5132a99371b3d1083a42fb783
felipefigueiredo1 <felipefigueiredosal@gmail.com>
2022-05-21 11:58:26 -0300

    feat

--- examples/create.php
+++ examples/create.php
@@ -5,7 +5,7 @@ require __DIR__."/../vendor/autoload.php";
 use App\Models\User;
 
 $user = new User();
-$user->nome = "Pedro";
+$user->nome = "Pedros";
 $user->sobrenome = "Brião";
 $user->cpf = "99920131";
 $user->telefone = "5521999830221";
